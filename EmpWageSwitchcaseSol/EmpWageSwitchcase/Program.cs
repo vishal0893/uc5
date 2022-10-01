caf5661c09135c5baa20d02409b28b972ee3488d
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-10-01 08:51:35 +0530

    Add files via upload

---
+++ EmpWageSwitchcaseSol/EmpWageSwitchcase/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EmpWage;
+class EmpWage
+{
+    static void Main(string[] args)
+    {
+        //constant
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
+        const int WAGE_PER_HOUR = 20;
+        const int NO_OF_WORKING_DAYS = 20;
+        //variables
+        int empHrs=0;
+        int empWage=0;
+        int totalEmpwage = 0;
+        for (int day = 0; day < NO_OF_WORKING_DAYS; day++)
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            switch (empCheck)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * WAGE_PER_HOUR;
+            totalEmpwage += empWage;
+            Console.Write("employe wage:" + empWage);
+        }
+        Console.Write("Total employee wage is" + totalEmpwage);
+
+    }
+}
---
+++ EmpWageSwitchcaseSol/EmpWageSwitchcase/uc6.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageSwitchcase
+{
+    internal class Empwageformonth
+    {
+        static void totalwage(string[] args)
+        {
+            //constant
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
+            const int WAGE_PER_HOUR = 20;
+            const int NO_OF_WORKING_DAYS = 20;
+            const int MAX_HRS_IN_A_MONTH = 100;
+            //variables
+            int empHrs = 0;
+            int Totalworkingdays = 0;
+            int totalEmphrs = 0;
+            while(empHrs <= MAX_HRS_IN_A_MONTH && Totalworkingdays < NO_OF_WORKING_DAYS)
+            {
+                Totalworkingdays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                
+                totalEmphrs += empHrs;
+                Console.Write("days:" + Totalworkingdays +"employee hours" +empHrs);
+            }
+            int Totalempwage = totalEmphrs * WAGE_PER_HOUR;
+            Console.Write("Total employee wage is" + Totalempwage);
+
+        }
+    }
+}
